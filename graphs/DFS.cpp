3b8a86a436fd6729d1d7fc8ffdb350594651e94d
Santos Alejandro Arellano Olarte <133918146+Santos-Arellano@users.noreply.github.com>
2023-11-07 14:17:26 -0600

    update

--- graphs/DFS.cpp
+++ graphs/DFS.cpp
@@ -6,68 +6,58 @@ using namespace std;
 
 class Graph {
     private:
-        int V; //number of vertices
-        list<int> *adj; //adj list que apunta a un array de listas
+        int V; // Número de vértices
+        list<int> *adj; // Lista de adyacencia que apunta a un array de listas
 
     public:
-        Graph(int V); //constructor
-        void addEdge(int v, int w); //funcion para agregar una arista al grafo donde 
-        void DFS(int s); //recorrido DFS a partir de un vertice s dond s significa "source"
+        Graph(int V); // Constructor
+        void addEdge(int v, int w); // Función para agregar una arista al grafo
+        void DFS(int s); // Recorrido DFS a partir de un vértice s
+        void DFSUtil(int v, bool visited[]); // Función de utilidad para el recorrido DFS
 }; 
 
-Graph::Graph(int V){ //constructor
-    this ->V = V; //inicializa el numero de vertices
-    adj = new list<int>[V]; //crea un array de listas de tamaño V
-
+Graph::Graph(int V) { // Constructor
+    this->V = V;
+    adj = new list<int>[V];
 }
 
-void Graph::addEdge(int v, int w){ 
-    adj[v].push_back(w); //agrega w a la lista de v donde w son vertices
-    //adj [w].push_back(v); //agrega v a la lista de w donde v son vertices
+void Graph::addEdge(int v, int w) { // Función para agregar una arista al grafo
+    adj[v].push_back(w);
 }
 
-void Graph::DFS(int s){
-    bool *visited = new bool[V]; //crea un array de booleanos de tamaño V
+void Graph::DFSUtil(int v, bool visited[]) { // Recorrido DFS a partir de un vértice s
+    visited[v] = true; // Marcar el vértice actual como visitado y mostrarlo
+    cout << v << " "; // Recorrer todos los vértices adyacentes al vértice actual
 
-    for (int i= 0; i < V; i++){
-        visited[i] = false; //inicializa todos los vertices como no visitados
-    }
-
-stack<int> stack; //crea una pila de tipo stack
-stack .push(s); //inserta el vertice de origen a la pila
-
-while(!stack.empty()) {
-    s = stack.top(); //saca un vertice de la pila
-    stack.pop(); //elimina el vertice de la pila
-
-    if (!visited[s]){
-        cout << s << " "; //imprime el vertice
-        visited[s] = true; //marca el vertice como visitado      
-    }
-    
-    for(list<int>::iterator i = adj[s].begin(); i != adj[s].end(); ++i){ //recorre todos los vertices adyacentes de s 
-        if (!visited[*i]){ //si no ha sido visitado 
-            stack.push(*i); //inserta el vertice adyacente de s si no ha sido visitado
+    list<int>::iterator i; // Recorrer todos los vértices adyacentes al vértice actual
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) { // Si un vértice adyacente no ha sido visitado, llamar recursivamente a la función DFSUtil para ese vértice
+        if (!visited[*i]) 
+            DFSUtil(*i, visited);
         }
     }
-}
 
-}
 
-int main(){
-    Graph g(5); //crea un grafo con 5 vertices
-    g.addEdge(0,1); //agrega las aristas
-    g.addEdge(0,2);
-    g.addEdge(1,0);
-    g.addEdge(2,0);
-    g.addEdge(1,2);
-    g.addEdge(1,4);
-    g.addEdge(2,1);
-    g.addEdge(2,3);
-    g.addEdge(3,2);
-    g.addEdge(3,4);
-    g.addEdge(4,1);
-
-    
-    g.DFS(0); //recorrido DFS a partir del vertice 0
+void Graph::DFS(int s) {
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++) {
+        visited[i] = false;
     }
+
+    DFSUtil(s, visited);
+}
+
+int main() {
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "DFS(2): ";
+    g.DFS(2);
+    cout << endl;
+
+    return 0;
+}
