10cd8815d7e12fd3ca1beb912eb7b47edb54109d
Santos_Arellano <Arellanosantoso6@gmail.com>
2023-11-28 00:19:32 -0600

    ggs

--- Act 5.2 - Actividad Integral Sobre El Uso De Códigos Hash (Evidencia Competencia)/main.cpp
+++ Act 5.2 - Actividad Integral Sobre El Uso De Códigos Hash (Evidencia Competencia)/main.cpp
@@ -14,37 +14,25 @@ string extractIp(string &line) {
 }
 
 long long stringToLongIP(const string& full_ip) {
-    size_t colonPos = full_ip.find(":");
-    if (colonPos != string::npos) {
-        string ip = full_ip.substr(0, colonPos);
-        string puerto = full_ip.substr(colonPos + 1);
-
-        // Convertir cada parte de la IP a entero y concatenarlas
-        stringstream ip_stream(ip);
-        int ip_part;
-        string ip_concatenada;
-        for (int i = 0; i < 4; i++) {
-            ip_stream >> ip_part;
-            ip_concatenada += to_string(ip_part);
-            if (i < 3) {
-                // Añadir un separador entre las partes de la IP
-                ip_concatenada += ".";
+        size_t colonPos = full_ip.find(":");
+        if (colonPos != string::npos) {
+            string ip = full_ip.substr(0, colonPos);
+            string puerto = full_ip.substr(colonPos + 1);
+            stringstream f(ip);
+            string ip_num, resultado;
+            for (int i = 0; i < 4; i++) {
+                getline(f, ip_num, '.');
+                resultado = resultado + ip_num;
             }
+            resultado = resultado + puerto;
+            long long ip_int = stoll(resultado);
+            return ip_int;
         }
-
-        // Concatenar la IP y el puerto
-        ip_concatenada += ":" + puerto;
-
-        // Convertir la IP completa a long long
-        long long ip_int = stoll(ip_concatenada);
-        return ip_int;
-    }
-    return 0;
+        return 0;
 }
 
 int main() {
     HashTable hashTable;
-
     ifstream inputFile("bitacora.txt");
     if (!inputFile.is_open()) {
         cerr << "Error al abrir el archivo.\n";
@@ -59,19 +47,21 @@ int main() {
 
     }
     inputFile.close();
-    hashTable.insert(3114984089,"Sep 24 17:22:12 311.49.840.89:4145 Failed password for illegal user guest");
-    // Imprimir datos para una IP específica
-    hashTable.print(3114984089);
-
+    //hashTable.insert(3114984089,"Sep 24 17:22:12 311.49.840.89:4145 Failed password for illegal user guest");
+    //hashTable.print(3114984089);
+    string ip_;
+    cout << "¿De cuál IP quieres obtener su resumen? (ejemplo:311.49.840.89:4145)" << endl;
+    cin>>ip_;
+    hashTable.print(stringToLongIP(ip_));
     return 0;
 }
+
 
 /*
 g++ -c LinkedList.cpp -o LinkedList.o
 g++ -c HashTable.cpp -o HashTable.o
 g++ -c main.cpp -o main.o
 g++ main.o HashTable.o LinkedList.o -o main
+./main
 
 */
-
-//./main
